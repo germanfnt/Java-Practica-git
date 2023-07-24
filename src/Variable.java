a791203d7e2c3e5cbad56f9f083249c5575d7ed9
german <germanfntorres@gmail.com>
2023-07-24 20:05:07 -0300

    pruebas de variables

--- src/Variable.java
+++ src/Variable.java
@@ -1,10 +1,24 @@
 public class Variable {
 
     public static void main (String[] args) {
+        // Ejercicio 1;
+        // definir las variables con sus respectivos valores;
+        // byte, short, int, long, char, boolean, String, double, float
 
-        System.out.println("Hola mundo");
+        // definicion de variables
+        byte varibleByte;
+        byte variable2Byte;
 
-        System.out.println("test git");
+
+        // asignacion de datos a variables
+        varibleByte = 100;
+        variable2Byte = 127;
+
+
+        //suma y muestra de variables
+
+        System.out.println("Mostrando suma de variables Byte: " + variable2Byte + varibleByte);
+
 
     }
 
